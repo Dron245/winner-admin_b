fdbc1ed0a60654fd6189335f7810c7a608c4011b
Dron245 <Dron245@yandex.ru>
2023-12-20 14:02:08 +0300

    2012-1402

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.addEventListener("click", documentActions);
+    function documentActions(e) {
+        const targetElement = e.target;
+        if (targetElement.closest(".form-select__item-first")) document.documentElement.classList.add("select-open");
+        if (!targetElement.closest(".form-select__item-first") && !targetElement.closest(".options")) document.documentElement.classList.remove("select-open");
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
